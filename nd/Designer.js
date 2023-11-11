40a92436bcba01ac18731d7c93cff32a43d8b23a
nopicus <127013029+nopicus@users.noreply.github.com>
2023-11-11 21:19:11 +0700

    Add files via upload

---
+++ nd/Designer.js
@@ -0,0 +1,13 @@
+import {React, useEffect, useState } from 'react';
+import {View, Text, TextInput, Image,TouchableOpacity,Pressable} from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useNavigation } from '@react-navigation/native';
+export default function Designer({}) {
+ return(
+    <View style={{flex:1 }}>
+        <Text style={{fontSize:25,fontFamily:'roboto'}}>Hi</Text>
+    </View>
+    
+ )
+}
